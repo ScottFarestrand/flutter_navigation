6484e14075ad12ee7af1f2897c25e252bcb5394e
ScottFarestrand <scottfarestrand@gmail.com>
2022-07-23 09:12:17 -0700

    Completed error handling of relationship reads

--- lib/models/people.dart
+++ lib/models/people.dart
@@ -1,26 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Relationship{
   String firstName;
   String lastName;
-  DateTime birthDate;
-  DateTime anniversaryDate;
+  Timestamp birthDate;
+  Timestamp anniversary;
   String relationshipType;
   int randomReminders;
   String parentUserID;
 
   Relationship({
-   required this.firstName,
-   required this.lastName,
-   required this.birthDate,
-   required this.anniversaryDate,
-   required this.relationshipType,
-   required this.randomReminders,
-   required this.parentUserID
+    required this.firstName,
+    required this.lastName,
+    required this.birthDate,
+    required this.anniversary,
+    required this.relationshipType,
+    required this.randomReminders,
+    required this.parentUserID
 });
   void updateRelationship(
     String firstName,
     String lastName,
-    DateTime birthDate,
-    DateTime anniversaryDate,
+    Timestamp birthDate,
+    Timestamp anniversary,
     String relationshipType,
     int randomReminders,
     String parentUserID,
@@ -29,7 +31,7 @@ class Relationship{
     firstName = firstName;
     lastName = lastName;
     birthDate = birthDate;
-    anniversaryDate = anniversaryDate;
+    anniversary = anniversary;
     relationshipType  = relationshipType;
     randomReminders = randomReminders;
     parentUserID = parentUserID;
@@ -39,20 +41,19 @@ class Relationship{
     'firstname': firstName,
     'lastname': lastName,
     'birthdate': birthDate,
-    'anniversarydate': anniversaryDate,
+    'anniersary': anniversary,
     'relationshiptype': relationshipType,
     'randomreminders': randomReminders,
     'userid': parentUserID,
   };
   static Relationship fromJson(Map<String, dynamic> json) => Relationship(
-    firstName: json.toString().contains("firstname") ? json['firstname'] : "",
-    // firstName: json['firstname'],
-    lastName: json['lastname'],
-    birthDate: json['birthdate'],
-    anniversaryDate: json['anniversarydate'],
-    relationshipType: json['relationshiptype'],
-    randomReminders: json['randomreminders'],
-    parentUserID: json['userid'],
+    firstName: json.toString().contains("firstname") ? json['firstname'] : "", // firstName: json['firstname'],
+    lastName: json.toString().contains("lastname") ? json['lastname'] : "",
+    birthDate: json.toString().contains("birthdate") ? json['birthdate'] : DateTime.now(),
+    anniversary: json.toString().contains("anniversary") ? json['anniversary'] : DateTime.now(),
+    relationshipType: json.toString().contains("relationshiptype") ? json['relationshiptype'] : "",
+    randomReminders: json.toString().contains("randomreminders") ? json['randomreminders']: 0,
+    parentUserID: json.toString().contains("userid") ? json['userid'] : "",
   );
 
 }
